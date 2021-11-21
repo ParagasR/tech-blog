ef46867eedca3a3872f5fb6bc68301c7e4d88cbf
NinjaStep <r.paragas@outlook.com>
2021-11-21 12:31:06 -0700

    started file structure

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+//change force to false when going live to retain data
+//force true will drop and then create the tables
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
