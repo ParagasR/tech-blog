f3c696dabf20bd4ad5a5bc0f468d786848a5da26
NinjaStep <r.paragas@outlook.com>
2021-11-22 22:48:47 -0700

    set up cookies an session

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
+//npm requireed stuff
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//files to require
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+//set up sessions w/ cookies
+const sess = {
+    secret: 'change this later to an environment variable',
+    cookie: {
+        //spec for cookie:
+        maxAge: 86400, //set it to exepire after 24 hours
+    },
+    //prevent the session to saved back to the session store
+    //session store: where it will be saved to, either memory or some database
+    //we dont want to save the session
+    resave: false,
+    //forces new unmodified sessions to be saved the session store, anything modified will not store
+    //might change false later so that it wont be stored either way? idk yet
+    saveUninitialized: true,
+    //set what kind of database is being used (MySQL)
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+}
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -18,7 +44,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //middleware to connect to the routes
-app.use(require('./controllers/'));
+app.use(routes);
 
 //change force to false when going live to retain data
 //force true will drop and then create the tables
