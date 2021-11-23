a572e02d0150f6ac38adb8d7c42ac7444233e22a
NinjaStep <r.paragas@outlook.com>
2021-11-22 22:55:47 -0700

    finished models/Posts

---
+++ models/Posts.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model { };
+
+Posts.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            //idk if this will be a PK or not, will keep it commentted until needed
+            // primaryKey:true,
+        },
+        post: {
+            //I think this will be a string? idk if there is a long version of this and will check up later on this, for now just set to a string
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts',
+    }
+);
+
+module.exports = Posts;
