db1705946488dc52f8784a41f0c4ceaedb213ca7
NinjaStep <r.paragas@outlook.com>
2021-11-29 04:27:19 -0700

    added get route for individual posts with comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
     //change to get data from database blog items then pass through blog template handlebar
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
         const posts = dbPostData.map((post) => post.get({ plain: true }));
         console.log(posts)
 
-        res.render('post', { posts })
+        res.render('post', { posts, loggedIn: req.session.loggedIn })
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
@@ -30,6 +30,30 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+})
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postDetails = await Post.findByPk(req.params.id, {
+            include: [{
+                model: Comment,
+                attributes: ['comment', 'createdAt'],
+                include: {
+                    model: User,
+                    attributes: ['user']
+                }
+            },
+            {
+                model: User,
+                attributes: ['user']
+            }]
+        });
+        const post = postDetails.get({ plain: true })
+        res.render('singlePost', post)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
